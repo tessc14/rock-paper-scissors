2c1ae0bb7ed0efda5fa783c398d7a1f7f84bdc8b
tessc14 <tessc14@gmail.com>
2023-01-15 15:09:55 +0000

    make game work

--- rps.js
+++ rps.js
@@ -8,11 +8,13 @@ const rps = () => {
     let our_input = arr[x];
     
     let result;
-//|| (user_input !== "rock" && user_input !== "paper" && user_input !== "scissors")
-    if (user_input.length === 0 || user_input === " " ) {
-        console.log("Please choose rock, paper or scissors");
-    } else if ((user_input == "rock") || (user_input == "scissors") || (user_input == "paper")) {
-        console.log(`I said: ${our_input}`);
+        if (user_input !== "rock" && user_input !== "paper" && user_input !== "scissors") {
+            console.log("Please choose rock, paper or scissors");
+    // if (user_input.length === 0 || user_input === " " ) {
+    //     console.log("Please choose rock, paper or scissors");
+    // } else if ((user_input == "rock") || (user_input == "scissors") || (user_input == "paper")) {
+        } else {
+            console.log(`I said: ${our_input}`);
             if (user_input == our_input) {
                 console.log("It's a draw");
             } else if ((user_input == "rock" && our_input == "scissors") || (user_input == "scissors" && our_input == "paper") || (user_input == "paper" && our_input == "rock")) {
@@ -20,12 +22,6 @@ const rps = () => {
             } else if ((user_input == "rock" && our_input == "paper") || (user_input == "scissors" && our_input == "rock") || (user_input == "paper" && our_input == "scissors")){
                 console.log("You lose");
             }
-    
-    } 
-        
-        // else {
-    //     console.log("Please choose rock, paper or scissors")
-    }
-
-
+    }    
+}
 rps();
