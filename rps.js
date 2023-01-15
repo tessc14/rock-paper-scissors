9a66e3223c8bd580ee03a02c2ba2ef2cc7beed8f
tessc14 <tessc14@gmail.com>
2023-01-15 15:46:26 +0000

    add replay option

--- rps.js
+++ rps.js
@@ -12,22 +12,31 @@ const rps = () => {
     let our_input = arr[x];
     
     let result;
-        if (user_input !== "rock" && user_input !== "paper" && user_input !== "scissors") {
-            console.log("Please choose rock, paper or scissors");
-        } else {
-            console.log(`I said: ${our_input}`);
-            if (user_input == our_input) {
-                draws ++;
-                console.log(c.blue("It's a draw"));
-            } else if ((user_input == "rock" && our_input == "scissors") || (user_input == "scissors" && our_input == "paper") || (user_input == "paper" && our_input == "rock")) {
-                wins ++;
-                console.log(c.green("You win!"));
-            } else if ((user_input == "rock" && our_input == "paper") || (user_input == "scissors" && our_input == "rock") || (user_input == "paper" && our_input == "scissors")){
-                losses ++;
-                console.log(c.red("You lose"));
-            }
-        }    
+    if (user_input !== "rock" && user_input !== "paper" && user_input !== "scissors") {
+        console.log("Please choose rock, paper or scissors");
+        return rps();
+    } else {
+        console.log(`I said: ${our_input}`);
+        if (user_input == our_input) {
+            draws ++;
+            console.log(c.blue("It's a draw"));
+        } else if ((user_input == "rock" && our_input == "scissors") || (user_input == "scissors" && our_input == "paper") || (user_input == "paper" && our_input == "rock")) {
+            wins ++;
+            console.log(c.green("You win!"));
+        } else if ((user_input == "rock" && our_input == "paper") || (user_input == "scissors" && our_input == "rock") || (user_input == "paper" && our_input == "scissors")){
+            losses ++;
+            console.log(c.red("You lose"));
+        }
+    }  
+    let display = prompt("Would you like to see your results so far? Enter yes or any other key for no   ").toLowerCase();
+    if (display === "yes") {
+        console.log(`Wins: $[wins], Losses $[losses], Draws $[draws]`)
+        }
+    let replay = prompt("Play again? Enter yes or enter any other key to quit   ").toLowerCase();
+    if (replay === "yes") {
+        return rps();
+    } else {
+    console.log("Goodbye!");
+    }
 }
-
-
 rps();
