b67bc8fd668b4d1f42c06926ea1d4e33fefdb7f5
tessc14 <tessc14@gmail.com>
2023-01-15 15:26:32 +0000

    display results

--- rps.js
+++ rps.js
@@ -3,6 +3,10 @@ const rps = () => {
     const prompt = require('prompt-sync')();
     const user_input = prompt('Rock, paper, scissors... ').toLowerCase();
 
+    let wins = 0;
+    let losses = 0;
+    let draws = 0;
+
     let arr = ["rock", "paper", "scissors"];
     let x = Math.floor((Math.random() * 3));
     let our_input = arr[x];
@@ -13,12 +17,17 @@ const rps = () => {
         } else {
             console.log(`I said: ${our_input}`);
             if (user_input == our_input) {
+                draws ++;
                 console.log(c.blue("It's a draw"));
             } else if ((user_input == "rock" && our_input == "scissors") || (user_input == "scissors" && our_input == "paper") || (user_input == "paper" && our_input == "rock")) {
+                wins ++;
                 console.log(c.green("You win!"));
             } else if ((user_input == "rock" && our_input == "paper") || (user_input == "scissors" && our_input == "rock") || (user_input == "paper" && our_input == "scissors")){
+                losses ++;
                 console.log(c.red("You lose"));
             }
         }    
 }
+
+
 rps();
